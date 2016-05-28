66861b51a6f4327e13afcc8b18fb33469bb5f5a0
bast3d0 <bast3d0@gmail.com>
2016-05-28 12:21:12 +0200

    Poruka

--- src/session/OvlascenjeDaoLocal.java
+++
@@ -1,8 +0,0 @@
-package session;
-
-import model.Ovlascenje;
-import rs.ac.uns.ftn.xws.sessionbeans.common.GenericDao;
-
-public interface OvlascenjeDaoLocal extends GenericDao<Ovlascenje, Integer> {
-
-}
