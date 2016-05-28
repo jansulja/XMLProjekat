f4edccd8eefcf35a1589a980774e95fa8127d109
bast3d0 <bast3d0@gmail.com>
2016-05-28 12:30:21 +0200

    jaxb parser

--- src/services/AktService.java
+++ src/services/AktService.java
@@ -1,5 +1,6 @@
 package services;
 
+import java.math.BigInteger;
 import java.util.List;
 
 import javax.ejb.EJB;
@@ -7,11 +8,19 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 
 import org.apache.log4j.Logger;
 
 import model.Akt;
+import model.Clan;
 import model.Gradjanin;
+import model.Podtacka;
+import model.Stav;
+import model.Tacka;
 import rs.ac.uns.ftn.xws.util.Authenticate;
 import session.AktDaoLocal;
 import util.AuthenticateGradjanin;
@@ -38,5 +47,28 @@ public class AktService {
 		}
 		return retVal;
     }
+	public void createXML (Akt akt){
+		try {
+			System.out.println("JAXB unmarshalling/marshalling.\n");
+			
+
+			// Definiše se JAXB kontekst (putanja do paketa sa JAXB bean-ovima)
+			JAXBContext context = JAXBContext.newInstance("model");
+			
+
+			// Marshaller je objekat zadužen za konverziju iz objektnog u XML model
+			Marshaller marshaller = context.createMarshaller();
+			
+			// Podešavanje marshaller-a
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+			
+			// Umesto System.out-a, može se koristiti FileOutputStream
+			marshaller.marshal(akt, System.out);
+			
+			
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+	}
 	
 }
